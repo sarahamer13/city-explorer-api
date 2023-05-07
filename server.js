4bad31bd23791fc1cfa2eb40fe75b7ce080fd0da
Sarah Mahdi <sacred13_heart@yahoo.com>
2023-05-06 23:36:33 -0700

    added lab7

---
+++ server.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// We bring dotenv
+require('dotenv').config();
+
+// We bring express
+const express = require('express');
+
+// We bring cors
+const cors = require('cors');
+
+// Bringing our data
+const weatherData = require('./data/weather.json');
+
+// Initializing express
+const app = express();
+
+// Middleware to allow open access with cors
+app.use(cors());
+
+const PORT = process.env.PORT;
+
+app.get('/', (request, response) => {
+  response.status(200).send('Hey your default route is working');
+});
+
+// http://localhost:3001/weatherData
+app.get('/weatherData', (request, response, next) => {
+  try {
+    const searchQuery = request.query.searchQuery;
+    console.log(searchQuery);
+
+    // Find city
+    const cityData = weatherData.find(weather => weather.city_name.toLowerCase() === searchQuery.toLowerCase());
+    console.log(cityData);
+
+    const formattedData = cityData.data.map(dayForecast => new Forecast(dayForecast));
+    response.status(200).send(formattedData);
+
+  } catch (error) {
+    next(error);
+  }
+});
+
+class Forecast {
+  constructor(weatherObjects) {
+    this.date = weatherObjects.valid_date;
+    this.description = weatherObjects.weather.description;
+    this.highTemp = weatherObjects.max_temp;
+    this.lowTemp = weatherObjects.low_temp;
+  }
+}
+
+app.get('*', (request, response) => {
+  response.send('The route was not found. Error 404');
+});
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
