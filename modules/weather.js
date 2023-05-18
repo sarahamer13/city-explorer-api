34c716209998354f842f82104d915c05cf941919
Sarah Mahdi <sacred13_heart@yahoo.com>
2023-05-17 20:51:15 -0700

    added cache

--- modules/weather.js
+++ modules/weather.js
@@ -1,25 +1,48 @@
 'use strict';
 
-const axios = require ('axios');
+const axios = require('axios');
+let cache = require('./cache.js');
+require('dotenv').config();
 
-function getWeather (request, response, next) {
-  const city = request.query.searchQuery;
-  const url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&city=${city}&days=5&units=I`;
+async function getWeather(latitude, longitude) {
+  const key = 'weather-' + latitude + '-' + longitude;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&days=5&lat=${latitude}&lon=${longitude}`;
+  console.log('Request URL:', url);
 
-  axios.get (url)
-    .then (response => response.data.data.map (weather => new Forecast(weather)))
-    .then (formattedData => response.status (200).send(formattedData))
-    .catch (err => next (err));
+  if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+    console.log('Cache hit');
+    console.log('Cached data:', cache[key].data);
+    console.log('Cached timestamp:', cache[key].timestamp);
+  } else {
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = axios.get(url)
+      .then(response => parseWeather(response.data));
+  }
 
+  return cache[key].data;
 }
 
-class Forecast {
-  constructor(weatherObjects) {
-    this.date = weatherObjects.valid_date;
-    this.description = weatherObjects.weather.description;
-    this.highTemp = weatherObjects.max_temp;
-    this.lowTemp = weatherObjects.low_temp;
+function parseWeather(weatherData) {
+  try {
+    const weatherSummaries = weatherData.data.map(day => {
+      return new Weather(day);
+    });
+    return Promise.resolve(weatherSummaries);
+  } catch (e) {
+    return Promise.reject(e);
   }
 }
+
+class Weather {
+  constructor(day) {
+    this.forecast = day.weather.description;
+    this.time = day.valid_date;
+  }
+}
+
+module.exports = getWeather;
+
 
 module.exports = getWeather;
